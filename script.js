e1423630315f739246a679b83fd5e3b9163c85e5
Shahriar Islam <85397003+ByteShahriar@users.noreply.github.com>
2024-01-07 14:10:55 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function displayItems(items) {
                 <tr><td colspan="2" class="entimage"><span class="imagecont"><a href="${entity.entityImage}"><img src="${entity.entityImage}" alt="${entity.entityName}" width="100px" height="130px"/></a></span></td></tr>
                 <tr><th class="enname"><strong>Name:</strong></th> <td><span>${entity.entityName}</span></td></tr>
                 <tr><th class="bnname"><strong>বাংলা:</strong></th> <td><span>${entity.entityNamebn}</span></td></tr>
-                <tr><th class="extype"><strong>Extinct Type:</strong></th> <td><span>${entity.entityExtinctType}</span></td></tr>
-                <tr><th class="sciname"><strong>Scientific Name:</strong></th> <td><span class="sciname">${entity.entitySciName}</span></td></tr>
+                <tr><th class="extype"><strong>Type:</strong></th> <td><span>${entity.entityExtinctType}</span></td></tr>
+                <tr><th class="sciname"><strong>Scientific:</strong></th> <td><span class="sciname">${entity.entitySciName}</span></td></tr>
             </table>
             <hr/>
         `;
