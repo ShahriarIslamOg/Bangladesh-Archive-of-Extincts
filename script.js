14de5ec76f706369afdaeecf74e6b3956558e83c
Shahriar Islam <85397003+ByteShahriar@users.noreply.github.com>
2024-01-07 12:53:12 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function displayJsonData(data) {
                 <tr><th class="enname"><strong>Name:</strong></th> <td><span>${entity.entityName}</span></td></tr>
                 <tr><th class="bnname"><strong>বাংলা:</strong></th> <td><span>${entity.entityNamebn}</span></td></tr>
                 <tr><th class="extype"><strong>Extinct Type:</strong></th> <td><span>${entity.entityExtinctType}</span></td></tr>
-                <tr><th class="sciname"><strong>Scientific Name:</strong></th> <td><span>${entity.entitySciName}</span></td></tr>
+                <tr><th class="sciname"><strong>Scientific Name:</strong></th> <td><span class="sciname">${entity.entitySciName}</span></td></tr>
             </table>
             <hr/>
         `;
