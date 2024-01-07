b5e7e9a017942cc78d83731e1663f41a91822144
Shahriar Islam <85397003+ByteShahriar@users.noreply.github.com>
2024-01-07 22:05:43 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -30,12 +30,12 @@ function displayItems(items) {
     items.forEach(entity => {
         extinctListDiv.innerHTML += `
             <table>
-                <tr><td class="entimage"><span class="imagecont"><a href="${entity.entityImage}"><img src="${entity.entityThumb}" alt="${entity.entityName}" width="150px" height="auto"/></a><a href="${entity.entityImageSource}">Image source</a> &#8599;</span></td></tr>
-                <tr><th class="enname"><strong>Name:</strong></th> <td><span>${entity.entityName}</span></td></tr>
-                <tr><th class="bnname"><strong>বাংলা:</strong></th> <td><span class="bn">${entity.entityNamebn}</span></td></tr>
-                <tr><th class="extype"><strong>Type:</strong></th> <td><span>${entity.entityExtinctType}</span></td></tr>
-                <tr><th class="sciname"><strong>Sci:</strong></th> <td><span class="sciname">${entity.entitySciName}</span></td></tr>
-                <tr><th class="lstseen"><strong>Seen:</strong></th> <td><span class="lstseen">${entity.entityLastSeen}</span></td></tr>
+                <tr><td class="entimage"><span class="imagecont"><a href="${entity.entityImage}"><img src="${entity.entityThumb}" alt="${entity.entityName}" width="150px" height="auto"/></a><a class="source" href="${entity.entityImageSource}">Image source</a> &#8599;</span></td></tr>
+                <tr><th class="enname"><strong>Name</strong></th> <td><span>${entity.entityName}</span></td></tr>
+                <tr><th class="bnname"><strong>বাংলা</strong></th> <td><span class="bn">${entity.entityNamebn}</span></td></tr>
+                <tr><th class="extype"><strong>Type</strong></th> <td><span>${entity.entityExtinctType}</span></td></tr>
+                <tr><th class="sciname"><strong>Sci</strong></th> <td><span class="sciname">${entity.entitySciName}</span></td></tr>
+                <tr><th class="lstseen"><strong>Seen</strong></th> <td><span class="lstseen">${entity.entityLastSeen}</span></td></tr>
             </table>
             <hr/>
         `;
